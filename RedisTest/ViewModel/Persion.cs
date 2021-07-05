7f9019ebc57a3904a2485e5c0635a88203adafcb
Jeffrey <780336375@qq.com>
2021-07-05 13:43:29 +0800

    Add model

---
+++ RedisTest/ViewModel/Persion.cs
@@ -0,0 +1,9 @@
+namespace RedisTest.ViewModel
+{
+    public record Persion
+    {
+        public string Id { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+    }
+}
