4b00e272f3ce8609e2f6b78af42b56f4aa671952
Jeffrey <yejian_1988@126.com>
2021-08-24 21:31:57 +0800

    Recursive traverse array

---
+++ RecursiveTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace RecursiveTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[6];
+            for(int i=0; i<nums.Length; i++)
+                nums[i] = i;
+            Postorder(nums, 0);
+        }
+
+        static void Preorder(int[] nums, int idx){
+            if(idx == nums.Length)
+                return;
+            Console.WriteLine($"num[{idx}]={nums[idx]}");
+            Preorder(nums, idx + 1);
+        }
+
+        static void Postorder(int[] nums, int idx){
+            if(idx == nums.Length)
+                return;
+            Postorder(nums, idx + 1);
+            Console.WriteLine($"num[{idx}]={nums[idx]}");
+        }
+    }
+}
