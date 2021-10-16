979a6d04b185bf6bf179333a102fe76b664653d4
Jeffrey <yejian_1988@126.com>
2021-10-16 16:16:09 +0800

    Explore int to char

---
+++ CharToInt/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CharToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = '0';
+            char c = (char)(1 + '0');
+            Console.WriteLine(c);
+        }
+    }
+}
