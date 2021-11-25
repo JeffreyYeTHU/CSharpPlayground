87a8a2c3a5ff32cf5a7754a48fb0e8e31a9bcadb
Jian <yejian_1988@outlook.com>
2021-11-25 15:31:44 +0800

    Add net 6 test project

--- LinqTest/Program.cs
+++ LinqTest/Program.cs
@@ -7,19 +7,46 @@ namespace LinqTest
     {
         static void Main (string[] args)
         {
-            Console.WriteLine ("Hello World!");
-            var s = new int[] { 1, 5, 3};
-            var sortedS = s.OrderByDescending(v => v).ToList();
-            foreach (var item in s)
+            //Console.WriteLine ("Hello World!");
+            //var s = new int[] { 1, 5, 3};
+            //var sortedS = s.OrderByDescending(v => v).ToList();
+            //foreach (var item in s)
+            //{
+            //    Console.Write(item + ",");
+            //}
+            //Console.WriteLine();
+            //foreach (var item in sortedS)
+            //{
+            //    Console.Write(item + ",");
+            //}
+            //Console.WriteLine();
+
+            DicesProbability(2);
+        }
+
+        public static double[] DicesProbability(int n)
+        {
+            double[] dp = new double[5 * n + 1];
+            for (int i = 0; i < 6; i++)
+                dp[i] = 1 / 6.0d;
+            for (int i = 2; i <= n; i++)
             {
-                Console.Write(item + ",");
+                int preLen = 5 * (i - 1) + 1;
+                double[] temp = new double[preLen];
+                for (int j = 0; j < preLen; j++)
+                {
+                    temp[j] = dp[j];
+                    dp[j] = 0;
+                }
+                for (int j = 0; j < preLen; j++)
+                {
+                    for (int k = j; k < j + 6; k++)
+                    {
+                        dp[k] += temp[j] / 6.0d;
+                    }
+                }
             }
-            Console.WriteLine();
-            foreach (var item in sortedS)
-            {
-                Console.Write(item + ",");
-            }
-            Console.WriteLine();
+            return dp;
         }
     }
 }
