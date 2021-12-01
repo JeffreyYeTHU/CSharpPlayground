abe7ab488eb62cb0a3ce1a0b5c905ff8156f5a9b
Jian <yejian_1988@outlook.com>
2021-12-01 09:47:59 +0800

    Add redis playground

---
+++ RedisPlayaround/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using StackExchange.Redis;
+
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect(
+            new ConfigurationOptions
+            {
+                EndPoints = { "localhost:6379" }
+            });
+
+var db = redis.GetDatabase();
+var pong = await db.PingAsync();
+Console.WriteLine(pong);
