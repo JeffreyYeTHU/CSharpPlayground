6913f303d0923b21efcecbebf51fc4a920118489
Jian <yejian_1988@outlook.com>
2021-12-01 14:28:48 +0800

    Trying docker contaner for RedisDemo

---
+++ RedisDemo/RedisDemo/Startup.cs
@@ -0,0 +1,56 @@
+using Microsoft.OpenApi.Models;
+
+namespace RedisDemo;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+
+        services.AddControllers();
+        services.AddSwaggerGen(c =>
+        {
+            c.SwaggerDoc("v1", new OpenApiInfo { Title = "RedisTest", Version = "v1" });
+        });
+
+        services.AddStackExchangeRedisCache(options =>
+        {
+                // pull connection string form appsetting.json
+                options.Configuration = Configuration.GetConnectionString("Redis");
+
+                // prefix this name to all key, to make key collision more rare; 
+                // However, it can be argue that, differnet app should use different cache?
+                options.InstanceName = "RedisTest_";
+        });
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RedisTest v1"));
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
