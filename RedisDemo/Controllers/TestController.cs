24025dc441f897265ddf56bf23d8cfcc334ee079
Jian <yejian_1988@outlook.com>
2021-12-03 11:01:44 +0800

    Redis ping test

--- RedisDemo/Controllers/TestController.cs
+++ RedisDemo/Controllers/TestController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace RedisDemo.Controllers
@@ -7,5 +7,24 @@ namespace RedisDemo.Controllers
     [ApiController]
     public class TestController : ControllerBase
     {
+        private readonly IConnectionMultiplexer _redis;
+
+        public TestController(IConnectionMultiplexer redis)
+        {
+            _redis = redis ?? throw new ArgumentNullException(nameof(redis));
+        }
+
+        /// <summary>
+        /// Redis ping round trip time
+        /// ref: https://docs.redis.com/latest/rs/references/client_references/client_csharp/
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("ping_redis_rtt")]
+        public async Task<string> PingRedisRtt()
+        {
+            var db = _redis.GetDatabase();
+            var pong = await db.PingAsync();
+            return pong.ToString();
+        }
     }
 }
