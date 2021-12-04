7dd26f4f5c2ac0b515c11aef1093455d4bef335d
Jeffrey <yejian_1988@126.com>
2021-12-04 14:39:30 +0800

    Add some redis basic types test

--- RedisDemo/Controllers/TestController.cs
+++ RedisDemo/Controllers/TestController.cs
@@ -44,16 +44,57 @@ namespace RedisDemo.Controllers
             return echo;
         }
 
-        ///// <summary>
-        ///// Test the list type of redis
-        ///// </summary>
-        ///// <param name="key"></param>
-        ///// <param name="values"></param>
-        ///// <returns></returns>
-        //[HttpPost("test_list_type")]
-        //public async Task<string> EchoList(string key, IList<int> values)
-        //{
-        //    var db = _redis.GetDatabase();
-        //}
+        /// <summary>
+        /// Test the list type of redis
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="values"></param>
+        /// <returns></returns>
+        [HttpPost("test_list_type")]
+        public async Task<List<int>> EchoList(string key, IList<int> values)
+        {
+           var db = _redis.GetDatabase();
+
+           // add values to the list
+           // these values will be pushed one by one
+           await db.ListRightPushAsync(key, values.Select(v => (RedisValue)v).ToArray());
+           await db.ListLeftPushAsync(key, values.Select(v => (RedisValue)v).ToArray());
+
+           // get data from the list
+           var single = await db.ListGetByIndexAsync(key, 0);
+           var list = await db.ListRangeAsync(key);
+           return list.Select(v => (int)v).ToList();
+        }
+
+        [HttpPost("test_hash_type")]
+        public async Task<Dictionary<string, string>> EchoHash(string key, Dictionary<string, string> values)
+        {
+            var db = _redis.GetDatabase();
+            foreach (var kv in values)
+            {
+                await db.HashSetAsync(key, kv.Key, kv.Value);
+            }
+            var hash = await db.HashGetAllAsync(key);
+            return hash.ToDictionary(k => (string)k.Name, v => (string)v.Value);
+        }
+
+        [HttpPost("test_set_type")]
+        public async Task<List<string>> EchoSet(string key, IList<string> values)
+        {
+            var db = _redis.GetDatabase();
+            await db.SetAddAsync(key, values.Select(v => (RedisValue)v).ToArray());
+            var set = await db.SetMembersAsync(key);
+            return set.Select(v => (string)v).ToList();
+        }
+
+        [HttpPost("test_sorted_set_type")]
+        public async Task<List<string>> EchoSortedSet(string key, IList<string> values)
+        {
+            var db = _redis.GetDatabase();
+            foreach (var value in values)
+                await db.SortedSetAddAsync(key, value, value.Length);
+            var set = await db.SortedSetRangeByRankAsync(key);
+            return set.Select(v => (string)v).ToList();
+        }
     }
 }
