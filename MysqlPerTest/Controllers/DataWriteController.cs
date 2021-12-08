c7f2002d96e48703197878600bb5443c809a076d
Jeffrey <780336375@qq.com>
2021-12-08 16:26:45 +0800

    Add mysql perf test project

---
+++ MysqlPerTest/Controllers/DataWriteController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MysqlPerTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataWriteController : ControllerBase
+    {
+        [HttpPost("write_data")]
+        public async Task WriteData()
+        {
+
+        }
+    }
+}
