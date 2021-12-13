a4c97132a24a3478430e7cb0ae104b945e961d69
Jian <yejian_1988@outlook.com>
2021-12-13 16:32:26 +0800

    Add a PriorityQueue test

---
+++ PriorityQueueTest/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var pq = new PriorityQueue<int, int>();
+for (int i = 0; i < 10; i++)
+{
+    //int p = Random.Shared.Next(0, 10);
+    int p = 4;
+    Console.WriteLine($"enqueue (i,p)=({i},{p})");
+    pq.Enqueue(i, p);
+}
+while (pq.Count > 0)
+    Console.WriteLine(pq.Dequeue());
