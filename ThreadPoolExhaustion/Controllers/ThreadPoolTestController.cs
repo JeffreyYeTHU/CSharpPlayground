913d2f4701cdb2bf996a787c5509a8801f5462ad
Jeffrey <780336375@qq.com>
2021-12-20 12:16:32 +0800

    Test thread pool exhaustion

---
+++ ThreadPoolExhaustion/Controllers/ThreadPoolTestController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ThreadPoolExhaustion.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThreadPoolTestController : ControllerBase
+    {
+        [HttpPost("get_int")]
+        public int Get()
+        {
+            //Thread.Sleep(TimeSpan.FromSeconds(60));
+            WaistCpu();
+            return Random.Shared.Next(100);
+        }
+
+        [HttpPost("get_int/async")]
+        public async Task<int> GetAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(60));
+            WaistCpu();
+            return Random.Shared.Next(100);
+        }
+
+        private void WaistCpu()
+        {
+            var start = DateTime.UtcNow;
+            int var = 0;
+            while (true)
+            {
+                if((DateTime.UtcNow - start).TotalMilliseconds > 100)
+                    break;
+                for (int i = 0; i < 10; i++)
+                    var++;
+            }
+        }
+    }
+}
