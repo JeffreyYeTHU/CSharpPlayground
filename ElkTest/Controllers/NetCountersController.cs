617ad45eee6add5e08894f815df5fd8be0c71c27
Jeffrey <780336375@qq.com>
2021-12-20 15:40:12 +0800

    Add elk tset project

---
+++ ElkTest/Controllers/NetCountersController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+
+namespace ElkTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NetCountersController : ControllerBase
+    {
+        [HttpPost("log_net_counters")]
+        public async Task LogNetCounter()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+        }
+    }
+}
