0ae8a1384dda0adc1c0969a2a4c0348019130d82
Your Name <you@example.com>
2022-01-13 14:58:29 +0800

    Priority queue test

---
+++ TestPq/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestPq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pq = new PriorityQueue<int, int>();
+            for(int i=0; i<10; i++)
+            {
+                pq.Enqueue(i, 10-i);
+            }
+            while(pq.Count > 0)
+            {
+                Console.WriteLine(pq.Dequeue());
+            }
+        }
+    }
+}
