ff0d3cf17d300bf21ee902426fc97d559e6530b5
Your Name <you@example.com>
2022-01-20 11:05:05 +0800

    MS interview 5th round project init

---
+++ MsTest5/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MsTest5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Solution{
+        
+    }
+}
