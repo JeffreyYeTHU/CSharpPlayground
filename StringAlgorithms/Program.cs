bb39a918a503c24322e12444577903b9bff34265
Your Name <you@example.com>
2022-01-21 11:59:38 +0800

    Index count sort

---
+++ StringAlgorithms/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace StringAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = new string[]{"jeff", "leon", "phillip", "john", "paul", "zander"};
+            int[] index = new int[]{1, 2, 0, 1, 0, 0};
+            int maxIdx = 2;
+            IndexCountSort.Sort(arr, index, maxIdx);
+        }
+    }
+}
