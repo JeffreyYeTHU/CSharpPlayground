160af3be954cc6faa644b083a5e631b4821b5946
Jian Ye <yejian@microsoft.com>
2022-12-19 16:35:07 +0800

    Add benchmarks

--- BenchmarkDemo/Program.cs
+++ BenchmarkDemo/Program.cs
@@ -1,6 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Jobs;
 using BenchmarkDotNet.Running;
+using Microsoft.Extensions.Logging;
 using System.Security.Cryptography;
 
 namespace BenchmarkDemo
@@ -13,6 +13,7 @@ namespace BenchmarkDemo
                 .Run(args);
     }
 
+    [MemoryDiagnoser]
     public class Md5VsSha256
     {
         private const int N = 10000;
@@ -32,5 +33,58 @@ namespace BenchmarkDemo
 
         [Benchmark]
         public byte[] Md5() => md5.ComputeHash(data);
+    }
+
+    [MemoryDiagnoser]
+    public class LoggerPerf
+    {
+        private static ILoggerFactory _loggerFactory = LoggerFactory.Create(builder =>
+        {
+            builder
+                .AddFilter("Default", LogLevel.Information);
+                //.AddConsole();
+        });
+
+        private static ILogger _logger = _loggerFactory.CreateLogger(nameof(LoggerPerf));
+
+        private static Random _rand = new Random(0);
+
+        [Benchmark]
+        public void Log_StringInterp()
+        {
+            int nxt = _rand.Next(1_000_000);
+            _logger.LogInformation($"The next number is {nxt} at Time:{DateTime.Now}");
+        }
+
+        [Benchmark]
+        public void Log_Structure()
+        {
+            int nxt = _rand.Next(1_000_000);
+            _logger.LogInformation("The next number is {Next} at Time:{Time}", nxt, DateTime.Now);
+        }
+
+        [Benchmark]
+        public void Log_StringInterp_Debug()
+        {
+            int nxt = _rand.Next(1_000_000);
+            _logger.LogDebug($"The next number is {nxt} at Time:{DateTime.Now}");
+        }
+
+        [Benchmark]
+        public void Log_Structure_Debug()
+        {
+            int nxt = _rand.Next(1_000_000);
+            _logger.LogDebug("The next number is {Next} at Time:{Time}", nxt, DateTime.Now);
+        }
+
+        [Benchmark]
+        public void Log_Structure_Debug_WithIf()
+        {
+            int nxt = _rand.Next(1_000_000);
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogDebug("The next number is {Next} at Time:{Time}", nxt, DateTime.Now);
+            }
+        }
     }
 }
