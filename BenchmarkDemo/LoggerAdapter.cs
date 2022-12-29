2695aa42d6cb9bfbcfc61be304b683a5c85c599d
Jian Ye <yejian@microsoft.com>
2022-12-29 15:39:39 +0800

    Add log perf benchmark cases

---
+++ BenchmarkDemo/LoggerAdapter.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace BenchmarkDemo
+{
+    public sealed class LoggerAdapter<T>
+    {
+        private readonly ILogger<T> _logger;
+
+        public LoggerAdapter(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogDebug<T1>(string template, T1 arg1)
+        {
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogInformation(template, arg1);
+            }
+        }
+
+        public void LogDebug<T1, T2>(string template, T1 arg1, T2 arg2)
+        {
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogInformation(template, arg1, arg2);
+            }
+        }
+
+        public void LogDebug<T1, T2, T3>(string template, T1 arg1, T2 arg2, T3 arg3)
+        {
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogInformation(template, arg1, arg2, arg3);
+            }
+        }
+
+        public void LogDebug(string template, object[] args)
+        {
+            if (_logger.IsEnabled(LogLevel.Debug))
+            {
+                _logger.LogInformation(template, args);
+            }
+        }
+    }
+}
--- BenchmarkDemo/Program.cs
+++ BenchmarkDemo/Program.cs
@@ -1,7 +1,6 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 using Microsoft.Extensions.Logging;
-using System.Security.Cryptography;
 
 namespace BenchmarkDemo
 {
@@ -14,28 +13,6 @@ namespace BenchmarkDemo
     }
 
     [MemoryDiagnoser]
-    public class Md5VsSha256
-    {
-        private const int N = 10000;
-        private readonly byte[] data;
-
-        private readonly SHA256 sha256 = SHA256.Create();
-        private readonly MD5 md5 = MD5.Create();
-
-        public Md5VsSha256()
-        {
-            data = new byte[N];
-            new Random(42).NextBytes(data);
-        }
-
-        [Benchmark]
-        public byte[] Sha256() => sha256.ComputeHash(data);
-
-        [Benchmark]
-        public byte[] Md5() => md5.ComputeHash(data);
-    }
-
-    [MemoryDiagnoser]
     public class LoggerPerf
     {
         private static ILoggerFactory _loggerFactory = LoggerFactory.Create(builder =>
@@ -45,7 +22,8 @@ namespace BenchmarkDemo
                 //.AddConsole();
         });
 
-        private static ILogger _logger = _loggerFactory.CreateLogger(nameof(LoggerPerf));
+        private static ILogger<LoggerPerf> _logger = _loggerFactory.CreateLogger<LoggerPerf>();
+        private static LoggerAdapter<LoggerPerf> _loggerAdapter = new LoggerAdapter<LoggerPerf>(_logger);
 
         private static Random _rand = new Random(0);
 
@@ -64,13 +42,6 @@ namespace BenchmarkDemo
         }
 
         [Benchmark]
-        public void Log_StringInterp_Debug()
-        {
-            int nxt = _rand.Next(1_000_000);
-            _logger.LogDebug($"The next number is {nxt} at Time:{DateTime.Now}");
-        }
-
-        [Benchmark]
         public void Log_Structure_Debug()
         {
             int nxt = _rand.Next(1_000_000);
@@ -85,6 +56,13 @@ namespace BenchmarkDemo
             {
                 _logger.LogDebug("The next number is {Next} at Time:{Time}", nxt, DateTime.Now);
             }
+        }
+
+        [Benchmark]
+        public void Log_Structure_Debug_Adapter()
+        {
+            int nxt = _rand.Next(1_000_000);
+           _loggerAdapter.LogDebug("The next number is {Next} at Time:{Time}", nxt, DateTime.Now);
         }
     }
 }
