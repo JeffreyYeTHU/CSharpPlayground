2513fc8dd900836fbbc5f75c3ba45ce58b25d9de
Jeffrey <you@example.com>
2023-02-11 17:38:34 +0800

    Process data

---
+++ ProcessMovieLenData/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+
+var oldFileName = "/Users/jeffrey/Downloads/ml-10M100K/ratings.dat";
+var newFileName = "/Users/jeffrey/Downloads/ml-10M100K/ratings_100k.dat";
+var lines = File.ReadLines(oldFileName);
+using StreamWriter writer = File.AppendText(newFileName);
+int cnt = 0;
+foreach (var line in lines)
+{
+    cnt++; 
+    var newLine = line.Replace("::", "$");
+    await writer.WriteLineAsync(newLine);
+    if (cnt > 100_000) break;
+}
